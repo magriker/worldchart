8618686e8537970fc70955805f5232e56979700a
magriker <magken7@gmail.com>
2025-07-21 00:53:57 +0900

    commit

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+
 export const Main = () => {
+  const [countryName, setCountryName] = useState<string>("");
+  const [country, setCountry] = useState();
+  console.log(country[0]);
+
+  const lowerCase = (string) => {
+    const lower = string.toLowerCase();
+    return lower;
+  };
+
+  useEffect(
+    function () {
+      const fetchCountry = async () => {
+        const res = await fetch(
+          `https://restcountries.com/v3.1/name/${countryName}`
+        );
+        const data = await res.json();
+        setCountry(data);
+      };
+      fetchCountry();
+    },
+    [countryName]
+  );
+
   return (
     <div className="main-container">
       <h1>World Chart</h1>
       <form action="">
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => setCountryName(lowerCase(e.target.value))}
+        />
+        {countryName}
+        {/* {country && } */}
       </form>
     </div>
   );
